c57e38dd3135b112e6f9c41a8489ad1180abc6f1
Kavya Sureja <kavyasureja05@gmail.com>
2026-02-21 12:11:41 +0530

    Initial commit - Smart Backup Automation Project

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PROJECT_DIR="/home/devps-user/devops-project/smart-backup"
+SOURCE_DIR="$PROJECT_DIR/source"
+BACKUP_DIR="$PROJECT_DIR/backup"
+LOG_DIR="$PROJECT_DIR/logs"
+LOG_FILE="$LOG_DIR/backup.log"
+
+mkdir -p "$SOURCE_DIR"
+mkdir -p "$BACKUP.DIR"
+mkdir -p "$LOG_DIR"
+
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="$BACKUP_DIR/backup_$DATE.tar.gz"
+
+echo "Backup started at $DATE" >> $LOG_FILE
+
+if [ -d "$SOURCE_DIR" ]; then
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+
+if [ $? -eq 0 ]; then
+echo "Backup successful: $BACKUP_FILE" >> $LOG_FILE
+else
+echo "Backup failed!" >> $LOG_FILE
+fi
+else
+echo "Source directory does not exist!" >> $LOG_FILE
+fi
+echo "------------------------------------" >> $LOG_FILE
+cd "$BACKUP_DIR"
+ls -1t | tail -n +11 | xargs -I {} rm -- {}
+
